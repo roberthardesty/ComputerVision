3ed7b0cfa93b0cfec8ea11866e4652807c6121f1
roberth <robert.hardesty@javs.com>
2017-03-24 21:42:58 -0400

    Made a new user control that will be used for incrementing and decrementing EmguCV parameters. Once this workflow is complete it will allow for the adjusting of sensitivity and more.

---
+++ JAVS.ComputerVision.UI/JAVS.ComputerVision.UI/Controls/LabeledIncrementor.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JAVS.ComputerVision.UI.Controls
+{
+    /// <summary>
+    /// Interaction logic for LabeledIncrementor.xaml
+    /// </summary>
+    public partial class LabeledIncrementor : UserControl
+    {
+        private int _numValue = 0;
+
+        public LabeledIncrementor()
+        {
+            InitializeComponent();
+            txtNum.Text = _numValue.ToString();
+        }
+
+        public int NumValue
+        {
+            get { return _numValue; }
+            set
+            {
+                _numValue = value;
+                txtNum.Text = value.ToString();
+            }
+        }
+
+        private void cmdUp_Click(object sender, RoutedEventArgs e)
+        {
+            NumValue++;
+        }
+
+        private void cmdDown_Click(object sender, RoutedEventArgs e)
+        {
+            NumValue--;
+        }
+
+        private void txtNum_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtNum == null)
+            {
+                return;
+            }
+
+            if (!int.TryParse(txtNum.Text, out _numValue))
+                txtNum.Text = _numValue.ToString();
+        }
+    }
+}
